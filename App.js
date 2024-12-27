23e611f0a90cac65df41444f095bc14166a608ac
Preetish <preetishvij@gmail.com>
2024-12-27 12:59:29 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * <div id="parent">
+ *  <div id="child">
+ *   <h1>I'm h1 tag</h1>
+ *   <h2>I'm h2 tag</h2>
+ *  </div>
+ * </div>   
+ */
+
+const parent = React.createElement(
+    "div", 
+    {id:"parent"}, 
+    React.createElement(
+        "div", 
+        {id:"child"}, 
+        [React.createElement(
+            "h1",
+            {},
+            "I'm h1 tag"
+        ),
+        React.createElement(
+            "h2",
+            {},
+            "I'm h2 tag"
+        )]
+    )
+)
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
